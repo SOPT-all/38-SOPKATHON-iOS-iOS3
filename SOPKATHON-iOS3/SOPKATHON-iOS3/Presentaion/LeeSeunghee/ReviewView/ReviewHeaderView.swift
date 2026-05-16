013cc8a05026fac98c5db4fde10ebdf618665106
seunghee <daalgwabyul@naver.com>
2026-05-17 04:44:19 +0900

    [feat] 회고 헤더 수정

--- SOPKATHON-iOS3/SOPKATHON-iOS3/Presentaion/LeeSeunghee/ReviewView/ReviewHeaderView.swift
+++ SOPKATHON-iOS3/SOPKATHON-iOS3/Presentaion/LeeSeunghee/ReviewView/ReviewHeaderView.swift
@@ -30,7 +30,7 @@ final class ReviewHeaderView: BaseUIView {
     
     override func setStyle() {
         headerLabel.do {
-            $0.text = "내 실수!"
+            $0.text = "대단해 카드"
             $0.font = .title_sb_16
             $0.textColor = .black
         }
